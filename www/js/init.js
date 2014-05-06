b79355c1736b8621d6a327316da53380ebcc88ca
Stephen Morgan <stephen@doublethink.co.nz>
2014-05-06 21:03:06 +1200

    Rolled bindings into same file and inside deviceready

--- www/js/init.js
+++ www/js/init.js
@@ -1,3 +1,95 @@
+// GPS successfully occured function
+var onSuccess = function(position) {
+alert('Latitude: '          + position.coords.latitude          + '\n' +
+      'Longitude: '         + position.coords.longitude         + '\n' +
+      'Altitude: '          + position.coords.altitude          + '\n' +
+      'Accuracy: '          + position.coords.accuracy          + '\n' +
+      'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+      'Heading: '           + position.coords.heading           + '\n' +
+      'Speed: '             + position.coords.speed             + '\n' +
+      'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+function initBindings(){
+  $.ajaxSetup ({
+    cache: false
+  });
+
+  //Facebook login button, redirects to #mainpage if connected
+  $(".FB-login").bind("click", function(event, ui){
+ 
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+            $.mobile.changePage($("#mainpage"));
+        } else {
+            alert('Unable to login');
+        }
+    },{ scope: "email" });
+  });
+
+  //Facebook logout button, redirects to #login screen
+  $(".FB-logout").bind("click", function(event, ui){
+    FB.logout(function(response){
+      $.mobile.changePage($("#login"));
+    });
+  });
+
+  //Slider button to report page, checks person is logged into Facebook
+  $(".report-button").bind("click", function(event, ui){
+    
+    FB.getLoginStatus(function(response){
+        if (response.status === 'connected') {
+            $.mobile.changePage($("#mainpage"));
+        } else {
+            $.mobile.changePage($("#login"));
+        }
+    });
+  });
+
+  //Button for reporting pests
+  $( "#send-report" ).bind( "click", function(event, ui) {
+
+    //Show loading image
+    $.mobile.loading( "show");
+
+    //Check user is logged into Facebook
+    FB.getLoginStatus(function(response){
+        if (response.status === 'connected') {
+            
+          navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+        } else {
+            alert("Not logged in");
+            $.mobile.changePage($("#login"));
+        }
+    });
+
+    $.mobile.loading("hide");
+    
+/*
+    alert("Sending report");
+    var jsonUrl = "http://judas.herokuapp.com/pestspotted";
+    var newQuote = { "author" : author, "text" : quote };
+    $.post(jsonUrl,newQuote, function(data) {
+      alert("Added quote number " + data.pos + " " + data.author + " " + data.text);
+    }, 'json');*/
+  });
+};
+
+// Initialisation functions
+function initjQuery(){
+  initBindings();
+};
+
+
+
+
 // deviceready event listener, loads FB connect and checks if signed in
 if (typeof CDV === 'undefined') {
         alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
@@ -26,33 +118,6 @@ if (typeof CDV === 'undefined') {
         } catch (e) {
             alert(e);
         }
+
+        initjQuery();
 }, false);
-
-// Center #center-button div in main page
-$(document).delegate("#mainpage", "pageshow", function(e,data){    
-    $('#center-report').css('margin-top',($(window).height() - $('[data-role=header]').height() - $('[data-role=footer]').height() - $('#index-content').outerHeight())/2);
-});
-
-// Center #center-button div in login page 
-$(document).delegate("#login", "pageshow", function(e,data){    
-    $('#center-button').css('margin-top',($(window).height() - $('[data-role=header]').height() - $('[data-role=footer]').height() - $('#index-content').outerHeight())/2);
-});
-
-
-// GPS successfully occured function
-var onSuccess = function(position) {
-alert('Latitude: '          + position.coords.latitude          + '\n' +
-      'Longitude: '         + position.coords.longitude         + '\n' +
-      'Altitude: '          + position.coords.altitude          + '\n' +
-      'Accuracy: '          + position.coords.accuracy          + '\n' +
-      'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-      'Heading: '           + position.coords.heading           + '\n' +
-      'Speed: '             + position.coords.speed             + '\n' +
-      'Timestamp: '         + position.timestamp                + '\n');
-};
-
-// onError Callback receives a PositionError object
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
